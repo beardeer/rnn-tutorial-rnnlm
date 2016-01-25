9df1e3a312037eaa0d0d95f6f6f8e6f1cd131d57
Xiaolu Xiong <xiaolu@AUTOREG-712552.WIFI.WPI.EDU>
2016-01-25 15:28:18 -0500

    more work on ASSISTments data

--- data_process.py
+++ data_process.py
@@ -1,7 +1,36 @@
 #! /usr/bin/env python
 
+import numpy as np
+
 def dkt_input_data_reader(input_file_name = './data/dkt_input_2.txt'):
-	pass
+	input_file = open(input_file_name, 'r')
+	train_data = []
+	test_data = []
+	for i in input_file.readlines():
+		data = i.strip().split(',')
+		data = map(int, data)
+		data = np.asarray(data)
+		data = data + 2
+		data = data.tolist()
+
+		if len(data) < 2:
+			pass
+		train = [0] + data[:-1]
+		test = data[1:] + [1]
+
+		if len(train) != len(test):
+			print 'input data error!'
+			print train
+			print test
+			raise ValueError
+
+		train_data.append(train)
+		test_data.append(test)
+
+	return train_data, test_data
 
 if __name__ == "__main__": 
-	pass
+	a,b = dkt_input_data_reader()
+
+	print len(a)
+	print len(b)
--- train-theano.py
+++ train-theano.py
@@ -12,9 +12,11 @@ from datetime import datetime
 from utils import *
 from rnn_theano import RNNTheano
 
-_VOCABULARY_SIZE = int(os.environ.get('VOCABULARY_SIZE', '8000'))
+from data_process import dkt_input_data_reader
+
+_VOCABULARY_SIZE = (111 + 2)  * 2
 _HIDDEN_DIM = int(os.environ.get('HIDDEN_DIM', '80'))
-_LEARNING_RATE = float(os.environ.get('LEARNING_RATE', '0.005'))
+_LEARNING_RATE = float(os.environ.get('LEARNING_RATE', '0.05'))
 _NEPOCH = int(os.environ.get('NEPOCH', '100'))
 _MODEL_FILE = os.environ.get('MODEL_FILE')
 
@@ -47,47 +49,55 @@ unknown_token = "UNKNOWN_TOKEN"
 sentence_start_token = "SENTENCE_START"
 sentence_end_token = "SENTENCE_END"
 
-# Read the data and append SENTENCE_START and SENTENCE_END tokens
-print "Reading CSV file..."
-with open('data/reddit-comments-2015-08.csv', 'rb') as f:
-    reader = csv.reader(f, skipinitialspace=True)
-    reader.next()
-    # Split full comments into sentences
-    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
-    # Append SENTENCE_START and SENTENCE_END
-    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
-print "Parsed %d sentences." % (len(sentences))
+# # Read the data and append SENTENCE_START and SENTENCE_END tokens
+# print "Reading CSV file..."
+# with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+#     reader = csv.reader(f, skipinitialspace=True)
+#     reader.next()
+#     # Split full comments into sentences
+#     sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+#     # Append SENTENCE_START and SENTENCE_END
+#     sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+# print "Parsed %d sentences." % (len(sentences))
     
-# Tokenize the sentences into words
-tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+# # Tokenize the sentences into words
+# tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
 
-# Count the word frequencies
-word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
-print "Found %d unique words tokens." % len(word_freq.items())
+# # Count the word frequencies
+# word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+# print "Found %d unique words tokens." % len(word_freq.items())
 
-# Get the most common words and build index_to_word and word_to_index vectors
-vocab = word_freq.most_common(vocabulary_size-1)
-index_to_word = [x[0] for x in vocab]
-index_to_word.append(unknown_token)
-word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+# # Get the most common words and build index_to_word and word_to_index vectors
+# vocab = word_freq.most_common(vocabulary_size-1)
+# index_to_word = [x[0] for x in vocab]
+# index_to_word.append(unknown_token)
+# word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
 
-print "Using vocabulary size %d." % vocabulary_size
-print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
+# print "Using vocabulary size %d." % vocabulary_size
+# print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
 
-# Replace all words not in our vocabulary with the unknown token
-for i, sent in enumerate(tokenized_sentences):
-    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+# # Replace all words not in our vocabulary with the unknown token
+# for i, sent in enumerate(tokenized_sentences):
+#     tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
 
 # Create the training data
-X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
-y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+x, y = dkt_input_data_reader()
+X_train = np.asarray(x)
+y_train = np.asarray(y)
 
-print X_train.shape
-print X_train[:3]
-print y_train.shape
-print y_train[:3]
+# print X_train.shape
+# print len(X_train[0])
+# print X_train[0]
+# print X_train[1]
+# print X_train[2]
 
-raw_input()
+# print y_train.shape
+# print len(y_train[0])
+# print y_train[0]
+# print y_train[1]
+# print y_train[2]
+
+# raw_input()
 
 
 model = RNNTheano(vocabulary_size, hidden_dim=_HIDDEN_DIM)
